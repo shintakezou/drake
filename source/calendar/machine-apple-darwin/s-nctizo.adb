0e0214702504eaffbd559e665104dc687170d2cb
ytomino <demoonlit@panathenaia.halfmoon.jp>
2018-08-07 08:54:08 +0900

    Unify s-nctizo.ads in POSIX (one for FreeBSD is used)

--- source/calendar/machine-apple-darwin/s-nctizo.adb
+++ source/calendar/machine-apple-darwin/s-nctizo.adb
@@ -1,18 +1,29 @@
+with Ada.Exception_Identification.From_Here;
+with System.Native_Calendar;
+with C.sys.types;
 with C.time;
 package body System.Native_Calendar.Time_Zones is
-   use type C.signed_long;
-
-   Time_Offset_Value : Time_Offset;
-
-   --  implementation
+   use Ada.Exception_Identification.From_Here;
+   use type C.sys.types.time_t;
 
    function UTC_Time_Offset (Date : Time) return Time_Offset is
-      pragma Unreferenced (Date);
+      --  FreeBSD does not have timezone variable
+      GMT_Time : aliased constant Native_Time :=
+         To_Native_Time (Duration (Date));
+      Local_TM_Buf : aliased C.time.struct_tm;
+      Local_TM : C.time.struct_tm_ptr;
+      Local_Time : aliased C.sys.types.time_t;
    begin
-      return Time_Offset_Value;
+      Local_TM := C.time.localtime_r (
+         GMT_Time.tv_sec'Access,
+         Local_TM_Buf'Access);
+      Local_Time := C.time.timegm (Local_TM);
+      if Local_Time = -1 then -- to pass negative UNIX time (?)
+         Raise_Exception (Time_Error'Identity);
+      end if;
+      return Time_Offset ((Local_Time - GMT_Time.tv_sec) / 60);
    end UTC_Time_Offset;
 
 begin
    C.time.tzset;
-   Time_Offset_Value := Time_Offset (C.time.timezone / (-60));
 end System.Native_Calendar.Time_Zones;
